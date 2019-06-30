0c9efad8b36c1b6274f70b429715da30dc1dbdcd
Adrian Gheorghe <adrian.gheorghe.dev@gmail.com>
2019-06-30 15:09:08 +0100

    Add Hex to Color RGBA

--- image.go
+++ image.go
@@ -1,6 +1,7 @@
 package processors
 
 import (
+	"errors"
 	"fmt"
 	"image"
 	"image/color"
@@ -84,4 +85,40 @@ func (processor *ImageProcessor) Inspect(sourcePath string) (ImageInfo, error) {
 // GetHexColor turns RGBA to HEX
 func (processor *ImageProcessor) GetHexColor(color color.RGBA) string {
 	return fmt.Sprintf("%02x%02x%02x", color.R, color.G, color.B)
+}
+
+// ParseHexColorFast turns RGBA to HEX
+func (processor *ImageProcessor) ParseHexColorFast(s string) (c color.RGBA, err error) {
+	c.A = 0xff
+
+	if s[0] != '#' {
+		return c, errors.New("Pixel color information is incorrect")
+	}
+
+	hexToByte := func(b byte) byte {
+		switch {
+		case b >= '0' && b <= '9':
+			return b - '0'
+		case b >= 'a' && b <= 'f':
+			return b - 'a' + 10
+		case b >= 'A' && b <= 'F':
+			return b - 'A' + 10
+		}
+		err = errors.New("Pixel color information is incorrect")
+		return 0
+	}
+
+	switch len(s) {
+	case 7:
+		c.R = hexToByte(s[1])<<4 + hexToByte(s[2])
+		c.G = hexToByte(s[3])<<4 + hexToByte(s[4])
+		c.B = hexToByte(s[5])<<4 + hexToByte(s[6])
+	case 4:
+		c.R = hexToByte(s[1]) * 17
+		c.G = hexToByte(s[2]) * 17
+		c.B = hexToByte(s[3]) * 17
+	default:
+		err = errors.New("Pixel color information is incorrect")
+	}
+	return
 }
